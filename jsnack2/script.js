572af2c5bca15f9b516567947d7c2392aa08fb56
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-26 16:07:22 +0100

    fine

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,21 +1,42 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-
-const invitati = ['giovanni', 'pippo', 'carmelo', 'maria', 'sara'];
-
 // ciclo for
+// const invitati = ['giovanni', 'pippo', 'carmelo', 'maria', 'sara'];
+
+
+// const nomeUtente = prompt('inserire proprio nome');
+
+
+// let find = false;
+// for (let index = 0; index < invitati.length; index++) {
+
+//      if (nomeUtente == invitati[index]) {
+//         find = true;
+//      }
+//  }
+
+// if (find == false) {
+// console.log('mi stai antipatico, non sei invitato');
+// } else {
+//  console.log('sei simpatico, sei invitato alla mia festa. prossimamente riceverai indicazioni su luogo ed ora. vestiti elegante');
+// }
+
+// ciclo while
+const invitati = ['giovanni', 'pippo', 'carmelo', 'maria', 'sara'];
 const nomeUtente = prompt('inserire proprio nome');
-
 let find = false;
-for (let index = 0; index < invitati.length; index++) {
+let i = 0;
 
-    if (nomeUtente == invitati[index]) {
+while (find == false && i < invitati.lenght) {
+    if (invitati[i] == nomeUtente) {
         find = true;
+        nome = invitati[i];
     }
+    i++;
 }
 
-if (find == false) {
-    console.log('mi stai antipatico, non sei invitato');
-} else {
+if (find == true) {
     console.log('sei simpatico, sei invitato alla mia festa. prossimamente riceverai indicazioni su luogo ed ora. vestiti elegante');
+} else {
+    console.log('mi stai antipatico, non sei invitato');
 }
